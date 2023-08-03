7298bf1d3560a8dce3df71ab723476a6527f1b78
AWS ParallelCluster user <ec2-user@ip-10-0-44-143.us-west-2.compute.internal>
2023-08-03 07:10:30 +0000

    Updated

--- release-file-system.sh
+++ release-file-system.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup find local/directory -type f -print0 | xargs -0 -n 1 sudo lfs hsm_release &
+nohup find  /fsx/fsx/DeepPhenotype_PBMC_ImageSet_YSeverin/ -type f -print0 | xargs -0 -n 1 sudo lfs hsm_release &
