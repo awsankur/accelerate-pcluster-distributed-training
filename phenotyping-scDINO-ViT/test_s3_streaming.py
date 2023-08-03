391eadbc074483f94c013098dd175bbaf2eb90ab
AWS ParallelCluster user <ec2-user@ip-10-0-44-143.us-west-2.compute.internal>
2023-08-03 19:16:12 +0000

    updated

--- phenotyping-scDINO-ViT/test_s3_streaming.py
+++ phenotyping-scDINO-ViT/test_s3_streaming.py
@@ -1,13 +1,18 @@
-from torchdata.datapipes.iter import IterableWrapper, S3FileLister
+from torchdata.datapipes.iter import IterableWrapper, S3FileLister, S3FileLoader
 
 s3_prefixes = IterableWrapper(['s3://pcluster-ml-workshop/DeepPhenotype_PBMC_ImageSet_YSeverin/'])
 
 dp_s3_urls = S3FileLister(s3_prefixes,request_timeout_ms=100,region='us-west-2')
 
-for d in dp_s3_urls:
-    print(d)
+dp_s3_files = S3FileLoader(dp_s3_urls)
+for url, fd in dp_s3_files: # Start loading data
+    data = fd.read()
+    import pdb;pdb.set_trace()
 
-import pdb;pdb.set_trace()
+#for d in dp_s3_urls:
+#    print(d)
+
+#import pdb;pdb.set_trace()
 
 #dp_s3_urls = s3_prefixes.list_files_by_s3(request_timeout_ms=100)
 
